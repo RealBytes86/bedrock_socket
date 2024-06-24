1739254da2ecc9a0cf4cf4c0c091356a2935b9eb
RealBytes86 <echtedeveloper@gmail.com>
2024-06-24 02:31:01 +0200

    reload

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,17 @@ fn main() {
 
     let server = ServerSocket::new("127.0.0.1".to_string(), 19132);
 
+    println!("Server is Running.");
+
     loop
     {
         if let Some((packet, src)) = server.read_packet() {
-            if packet[0] == message_identifier::ID_UNCONNECTED_PING {
-                println!("{:?}", packet)
+
+            let mut packet_id: u8 = packet[0];
+
+            match packet_id {
+                message_identifier::ID_UNCONNECTED_PING => println!("{:?}", packet),
+                _ => println!("Unbekannte Packet {:?}", packet)
             }
 
         }
